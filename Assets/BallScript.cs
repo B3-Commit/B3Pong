9fad4e65c13648f6a706142e87a446158acc9de7
arvidskarrie <44729270+arvidskarrie@users.noreply.github.com>
2023-10-03 11:05:50 +0200

    Added paddle limits and ball minimum velocity

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -1,25 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using System.Math;
 
 public class Ball : MonoBehaviour
 {
     // public Rigidbody2D rigidbody
     // Start is called before the first frame update
+
+    public float velocity = 5.0f;
     void Start()
     {
         Debug.Log("start");
         var rigidBody = GetComponent<Rigidbody2D>();
 
-        float x = Random.Range(0, 2) == 0 ? -3 : 3;
-        float y = Random.Range(0, 2) == 0 ? -3 : 3;
+        float x = Random.Range(4f, 5f);
+        float y = 5.0f - (float)(System.Math.Sqrt(System.Math.Abs(x)));
+
         rigidBody.velocity = new Vector2(x, y);
     }
-
 
     // Update is called once per frame
     void Update()
     {
-        
+        var rigidBody = GetComponent<Rigidbody2D>();
+        var magnitude = rigidBody.velocity.magnitude;
+        if (magnitude < 4.5)
+        {
+            rigidBody.velocity = rigidBody.velocity / magnitude * velocity;
+        }
     }
 }
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -6,6 +6,9 @@ public class PlayerScript : MonoBehaviour
 {
     public KeyCode up = KeyCode.UpArrow;
     public KeyCode down = KeyCode.DownArrow;
+    public KeyCode left = KeyCode.LeftArrow;
+    public KeyCode right = KeyCode.RightArrow;
+    public float angularAcceleration = 1.0f;
 
     [SerializeField] float speed = 8.0f;
 
@@ -13,20 +16,41 @@ public class PlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+            var sprite = GetComponent<SpriteRenderer>().sprite.bounds;
+            var boxColl = GetComponent<BoxCollider2D>();
+            boxColl.size = sprite.size;
+            // boxColl.offset = sprite.center;
+            
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        var rigidBody = GetComponent<Rigidbody2D>();
         if (Input.GetKey(up))
         {
+            rigidBody.velocity = Vector2.zero;
             transform.position += Vector3.up * speed * Time.deltaTime;            
         }
         if (Input.GetKey(down))
         {
+            rigidBody.velocity = Vector2.zero;
             transform.position += Vector3.down * speed * Time.deltaTime;
         }
+        if (Input.GetKey(left))
+        {
+            rigidBody.angularVelocity += angularAcceleration;
+            // transform.RotateAround(new Vector3(0, 0, 1), 0.01f);
+        }
+        if (Input.GetKey(right))
+        {
+            rigidBody.angularVelocity -= angularAcceleration;
+            // transform.RotateAround(new Vector3(0, 0, 1), -0.01f);
+        }
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.5f, 3.5f), 0);
+        rigidBody.angularVelocity = Mathf.Clamp(rigidBody.angularVelocity, -1000f, 1000f);
         
     }
 }
