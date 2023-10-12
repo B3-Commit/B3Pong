96c6db7bc8765ce6936688f31c88c89eff8b62f1
arvidskarrie <44729270+arvidskarrie@users.noreply.github.com>
2023-10-12 15:28:09 +0200

    Add Action curve to camera shake

--- Assets/Resources/Animations/CameraShaker.cs
+++ Assets/Resources/Animations/CameraShaker.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
+    public AnimationCurve expandCurve;
     public Vector3 originalPosition;
     public float duration;
     public float magnitude;
@@ -14,8 +15,9 @@ public class CameraShake : MonoBehaviour
 
         while (elapsedTime < duration)
         {
-            float xOffset = Random.Range(-0.5f, 0.5f) * magnitude;
-            float yOffset = Random.Range(-0.5f, 0.5f) * magnitude;
+            float curveValue = expandCurve.Evaluate(elapsedTime / duration);
+            float xOffset = Random.Range(-0.5f, 0.5f) * magnitude * curveValue;
+            float yOffset = Random.Range(-0.5f, 0.5f) * magnitude * curveValue;
             transform.localPosition = new Vector3(xOffset, yOffset, originalPosition.z);
             elapsedTime += Time.deltaTime;
 
